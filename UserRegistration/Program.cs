202a9c1c69552da0dbc04f26310865e8d5e3381d
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-21 10:25:54 -0600

    feat: Entering the valid first name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -8,6 +8,11 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to user registration programs");
+            
+            Registration user = new Registration();
+            Console.Write("Enter your first name : ");
+            string firstName = Console.ReadLine();
+            user.Check(firstName);
         }
     }
 }
---
+++ UserRegistration/Registration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace UserRegistration
+{
+    class Registration
+    {
+        public void Check(string name)
+        {
+            string pattern = "^[A-Z][a-z]*$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine("{0} is Valid ", name);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Invalid", name);
+            }
+        }
+    }
+}
